950bbd6e2bc97607bd4665aa6cd2a2b34c9e81e1
Bailleul Loïc <gandbri@gmail.com>
2016-08-18 11:24:40 +0100

    Cleaned up unused comment

--- LDVELH_WPF/Data/MySQliteDbContext.cs
+++ LDVELH_WPF/Data/MySQliteDbContext.cs
@@ -5,12 +5,6 @@
 
     public class MySQLiteDBContext : DbContext
     {
-        // Your context has been configured to use a 'StoryContext' connection string from your application's 
-        // configuration file (App.config or Web.config). By default, this connection string targets the 
-        // 'LDVELH_WindowsForm.StoryContext' database on your LocalDb instance. 
-        // 
-        // If you wish to target a different database and/or database provider, modify the 'StoryContext' 
-        // connection string in the application configuration file.
         public MySQLiteDBContext()
             : base("HeroSQLiteContext")
         {
