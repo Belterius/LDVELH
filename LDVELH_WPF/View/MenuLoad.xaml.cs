f9ff3d80e949b2e87fbe8dcb4fe8722df2b3a99c
Belterius <gandbri@gmail.com>
2016-09-13 11:51:53 +0100

    Binded MenuLoad

--- LDVELH_WPF/View/MenuLoad.xaml.cs
+++ LDVELH_WPF/View/MenuLoad.xaml.cs
@@ -9,7 +9,6 @@ namespace LDVELH_WPF
     /// </summary>
     public partial class MenuLoad : Window
     {
-        List<Hero> listHeroes;
         public MenuLoad()
         {
             InitializeComponent();
@@ -23,68 +22,8 @@ namespace LDVELH_WPF
         }
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            try
-            {
-                using (SQLiteDatabaseFunction databaseRequest = new SQLiteDatabaseFunction())
-                {
-                    listHeroes = databaseRequest.GetAllHeroes();
-                }
-                listBoxHeroes.ItemsSource = listHeroes;
-                listBoxHeroes.DisplayMemberPath = "getResume";
-                listBoxHeroes.SelectedValuePath = "CharacterID";
-            }
-            catch (Exception ex)
-            {
-                System.Diagnostics.Debug.WriteLine("Error when loading all heroes data : " + ex);
-                buttonLoad.Visibility = Visibility.Hidden;
-            }
-           
         }
-
-        private void buttonNew_Click(object sender, RoutedEventArgs e)
-        {
-            MainWindow mainWindow = new MainWindow();
-            this.Close();
-        }
-
-        private void buttonLoad_Click(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                MainWindow mainWindow = new MainWindow((Hero)listBoxHeroes.SelectedItem);
-                mainWindow.Show();
-                this.Close();
-            }
-            catch (Exception ex)
-            {
-                System.Diagnostics.Debug.WriteLine("Error when loading hero data : " + ex);
-            }
-            
-        }
-
-        private void ButtonSettings_Click(object sender, RoutedEventArgs e)
-        {
-            MenuSettings menuSetting = new MenuSettings();
-            menuSetting.Show();
-            this.Close();
-        }
-
-        private void ButtonDelete_Click(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                using (SQLiteDatabaseFunction databaseRequest = new SQLiteDatabaseFunction())
-                {
-                    databaseRequest.DeleteHero((Hero)listBoxHeroes.SelectedItem);
-                }
-                listHeroes.Remove((Hero)listBoxHeroes.SelectedItem);
-                listBoxHeroes.Items.Refresh();
-                
-            }
-            catch (Exception ex)
-            {
-                System.Diagnostics.Debug.WriteLine("Error when loading hero data : " + ex);
-            }
-        }
+        
+        
     }
 }
---
+++ LDVELH_WPF/ViewModel/MenuLoadViewModel.cs
@@ -0,0 +1,110 @@
+﻿using LDVELH_WPF.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LDVELH_WPF.ViewModel
+{
+    class MenuLoadViewModel : ViewModelBase
+    {
+        Hero _SelectedHero;
+        public Hero SelectedHero
+        {
+            get
+            {
+                return _SelectedHero;
+            }
+            set
+            {
+                if (_SelectedHero != value)
+                {
+                    _SelectedHero = value;
+                    RaisePropertyChanged("SelectedHero");
+                }
+            }
+        }
+        ObservableCollection<Hero> _ListHeroes;
+        public ObservableCollection<Hero> ListHeroes
+        {
+            get
+            {
+                return _ListHeroes;
+            }
+            set
+            {
+                _ListHeroes = value;
+            }
+        }
+        private void LoadHeroes()
+        {
+            try
+            {
+                using (SQLiteDatabaseFunction databaseRequest = new SQLiteDatabaseFunction())
+                {
+                    ListHeroes = new ObservableCollection<Hero>(databaseRequest.GetAllHeroes());
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Error when loading all heroes data : " + ex);
+                //buttonLoad.Visibility = Visibility.Hidden;
+            }
+        }
+        public MenuLoadViewModel()
+        {
+            DeleteHeroCommand = new RelayCommand(DeleteHero);
+            LoadHeroCommand = new RelayCommand(LoadHero);
+            NewGameCommand = new RelayCommand(NewGame);
+            SettingsCommand = new RelayCommand(Settings);
+            LoadHeroes();
+        }
+        public RelayCommand DeleteHeroCommand { get; set; }
+        public RelayCommand LoadHeroCommand { get; set; }
+        public RelayCommand NewGameCommand { get; set; }
+        public RelayCommand SettingsCommand { get; set; }
+
+        private void DeleteHero(object hero)
+        {
+            try
+            {
+                using (SQLiteDatabaseFunction databaseRequest = new SQLiteDatabaseFunction())
+                {
+                    databaseRequest.DeleteHero((Hero)hero);
+                }
+                ListHeroes.Remove((Hero)hero);
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Error when loading hero data : " + ex);
+            }
+        }
+        private void LoadHero(object hero)
+        {
+            try
+            {
+                MainWindow mainWindow = new MainWindow((Hero)hero);
+                mainWindow.Show();
+                CloseWindow();
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Error when loading hero data : " + ex);
+            }
+        }
+
+        private void NewGame(object random)
+        {
+            MainWindow mainWindow = new MainWindow();
+            CloseWindow();
+        }
+        private void Settings(object random)
+        {
+            MenuSettings menuSetting = new MenuSettings();
+            menuSetting.Show();
+            CloseWindow();
+        }
+    }
+}
